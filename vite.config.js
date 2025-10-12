c0f75429ee94b17dbbe794aeb94ea9bde9244129
kevindbannister <kevin@kevindbannister.co.uk>
2025-10-12 16:42:27 +0100

    Build docs output for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Content-Builder/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
